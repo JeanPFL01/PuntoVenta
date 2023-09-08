884951826afc4fd1123458bcae6bfc5e5c43bb2e
JeanPFL01 <144464040+JeanPFL01@users.noreply.github.com>
2023-09-08 10:15:32 -0500

    PUSH 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent},
-  {path:'login', component:LoginComponent}
+  {path:'', component:LoginComponent}
 
 ];
 
